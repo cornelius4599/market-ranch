ee95afec3e3597a70e7be8af76bd1b79d038eec1
Cornexology <adedayocornex@gmail.com>
2024-05-08 10:36:57 +0100

    some tailwind

--- app/page.js
+++ app/page.js
@@ -2,6 +2,6 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-   <h1>Market Ranch</h1>
+   <h1 className="text-4xl text-center text-yellow-400 font-extrabold mt-10">Market Ranch</h1>
   );
 }
